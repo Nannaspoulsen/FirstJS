f93f510ade7abbce6ddbe0e505396c13ce1513db
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-02-06 13:17:53 +0100

    Øvelser

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
-// //const myName = 'Nanna';
+// OPGAVE !: 
+// const myName = 'Nanna';
 // //console.log('myName:', myName)
 // //let myAge = 34;
 // console.log('myAge:', myAge)
 // let myShoeSize = 40;
 // console.log('myShoeSize:', myShoeSize)
 
+
+
+
+// OPGAVE 2:
 // log("Hejsa jeg kalder den her funktion for mormor")
 // log("og morfar")
 
@@ -17,6 +22,33 @@
 //     bodyEl.innerHTML += input + "<br>";
 // }
 
-const poetry = "Fædreland, Plet af Jord paa Jorden; Fædreland,Du, vort Sekels Polen, indad sønderslidt og udad sønderlemmet; Fædreland, sørgeklædte Moder, lær os: smaa at være. Lær med Taalmod os Hverdagsdragt at bære Arbejdsdragt — Moder, dig til Ære."
 
-poetry.includes("Fædreland")
+
+//OPGAVE 3:
+// Du skal skrive kode der tjekker om ovenstående string indeholder ordet "Fædreland"
+
+// Kig i w3schools referenceliste og find en egnet metode
+
+// Du skal logge resultatet. Det betyder at du skal logge "false" hvis poetry IKKE indeholder ordet "Fædreland" og "true" hvis den gør.
+
+// Ekstra: Find ud af hvordan du kan VISE resultatet false/true så man kan se det på skærmen, f.eks. i <body> (ikke i konsollen)
+
+// const poetry = "Fædreland, Plet af Jord paa Jorden; Fædreland,Du, vort Sekels Polen, indad sønderslidt og udad sønderlemmet; Fædreland, sørgeklædte Moder, lær os: smaa at være. Lær med Taalmod os Hverdagsdragt at bære Arbejdsdragt — Moder, dig til Ære."
+
+// poetry.includes("Fædreland");
+// console.log(test);
+// const bodyEl = document.querySelector("body");
+// bodyEl.innerHTML = test;
+
+// Fattede ingenting, det hele er forkert.
+
+
+//OPGAVE 4:
+
+// const Number = Math.round(Math.sqrt(65));
+// console.log('Number', Number)
+
+// //Fik det til at virke og forstår godt hvordan det virker, men kan ikke helt se hvad jeg skal bruge det til
+
+//OPGAVE 5:
+
