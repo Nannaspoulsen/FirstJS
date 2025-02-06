b544a357485e1df2e6da62bd93c53d4ab399b9c4
Nannaspoulsen <Nannaspoulsen@hotmail.com>
2025-02-06 12:11:48 +0100

    øvelse

--- assets/js/.js
+++
@@ -1,7 +0,0 @@
-const myName = 'Nanna';
-console.log('myName:', myName)
-let myAge = 34;
-console.log('myAge:', myAge)
-let myShoeSize = 40;
-console.log('myShoeSize:', myShoeSize)
-
---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// //const myName = 'Nanna';
+// //console.log('myName:', myName)
+// //let myAge = 34;
+// console.log('myAge:', myAge)
+// let myShoeSize = 40;
+// console.log('myShoeSize:', myShoeSize)
+
+// log("Hejsa jeg kalder den her funktion for mormor")
+// log("og morfar")
+
+// function log(input) {
+
+//     //fanger body elementet
+//     const bodyEl = document.querySelector("body");
+
+//     //sætter dens inter HTML til en tekst
+//     bodyEl.innerHTML += input + "<br>";
+// }
+
+const poetry = "Fædreland, Plet af Jord paa Jorden; Fædreland,Du, vort Sekels Polen, indad sønderslidt og udad sønderlemmet; Fædreland, sørgeklædte Moder, lær os: smaa at være. Lær med Taalmod os Hverdagsdragt at bære Arbejdsdragt — Moder, dig til Ære."
+
+poetry.includes("Fædreland")
